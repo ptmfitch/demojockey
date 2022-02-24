58eb2bca8b24a10059aecbf24b57d6478f1e0613
Peter Fitch <ptmfitch@users.noreply.github.com>
2022-02-24 18:11:41 +0000

    restructured proofs command and added more robust error handling

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # demojockey README
 
-Demo Jockey is an internal MongoDB VSCode extension designed to make it easy for SAs and others to copy demo scripts and files from different sources into a VSCode project.
+Demo Jockey is a Visual Studio Code extension designed to simplify copying common scripts and files from various repositories. It is for internal MongoDB usage only.
 
 ## Features
+
+Adds the 'Proof Point' command to Visual Studio Code.
+- Use ⌘⇧P to open the command interface
+- Type 'Proof Point' in the dialogue box, it should autocomplete
+- Hit enter
+- Select a Proof Point from the list
+
 
 ⌘+⇧+P 'Proof Point': Shortcut to select from a list of POV Proof Points to copy into your open VSCode project.
 
 ## Release Notes
 
-### 0.0.1
+### 0.0.2
 
-Initial release of Demo Jockey includes POV Proof Point shortcut.
+Added POV Proof Exercises command
+Added configuration for local repository paths
+Added error handling for common occurrences
+Added informative messages to aid users
--- src/extension.ts
+++ src/extension.ts
@@ -2,55 +2,70 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-  const prfsPath = '/src/pov-proof-exercises/proofs';
-  const trgtPath = '/pov-proof-exercises/proofs';
+  let proofPointCommand = vscode.commands.registerCommand('demojockey.proofPoint', async () => {
 
-  let disposable = vscode.commands.registerCommand('demojockey.proofPoint', async () => {
+    const djConfig = vscode.workspace.getConfiguration('demojockey');
+    const pathsConfig = vscode.workspace.getConfiguration('demojockey.paths');
+    
+    const proofsDir = 'proofs';
+    const proofsRepo = pathsConfig.get('pov-proof-exercises', '');
+    const proofsPath = `${proofsRepo}/${proofsDir}`;
+    
+    const targetDir = 'pov-proof-exercises/proofs';
+    const doOverwrite = djConfig.get('overwrite', false);
 
-    if (vscode.workspace.workspaceFolders === undefined) {
-      vscode.window.showErrorMessage("Working folder not found, open a folder and try again");
+    async function listProofs(path: string) {
+      return (await vscode.workspace.fs.readDirectory(vscode.Uri.parse(path)))
+        .map(tuple => tuple[0])
+        .filter(dir => dir !== '.DS_Store');
+    }
+
+    function copyProofThenOpenReadme(source: vscode.Uri, target: vscode.Uri, overwrite: boolean) {
+      
+      const readmeFilename = 'README.md';
+      const readmePath = vscode.Uri.parse(`${target}/${readmeFilename}`).fsPath;
+
+      vscode.workspace.fs.copy(source, target, {overwrite: overwrite}).then(_ => {
+        vscode.workspace.openTextDocument(readmePath).then((td: vscode.TextDocument) => {
+          vscode.window.showTextDocument(td, 1, false);
+        }, (_) => {
+          const message = `Could not open ${readmePath}`;
+          vscode.window.showErrorMessage(message);
+        });
+      }, (_) => {
+        const message = 'Could not copy proof, it may already exist (overwrite configurable in Settings (⌘,))';
+        vscode.window.showErrorMessage(message);
+      }); 
+
+    }
+
+    if (!proofsRepo) {
+      const message = 'Could not access local pov-proof-exercises repository, please configure in Settings (⌘,)';
+      vscode.window.showErrorMessage(message);
       return;
     }
-    var rootPath = vscode.workspace.workspaceFolders[0].uri.path;
+    const proofsList = listProofs(proofsPath);
 
-    if (vscode.extensions.getExtension('ptmfitch.demojockey') !== undefined) {
-
-      var extnPath = vscode.extensions.getExtension('ptmfitch.demojockey')?.extensionUri.path;
-
-      // List proof directories from the repo
-      var proofs = (await vscode.workspace.fs.readDirectory(vscode.Uri.parse(extnPath + prfsPath))).map(t => t[0]);
-
-      // User input to select the directory
-      const result = await vscode.window.showQuickPick(proofs, {
-        placeHolder: 'Select a Proof Point',
-      });
-      vscode.window.showInformationMessage(`Selected ${result}`);
-      
-      // Copy the directory into the project
-      var source: vscode.Uri = vscode.Uri.parse(extnPath + `${prfsPath}/${result}`);
-      var target: vscode.Uri = vscode.Uri.parse(rootPath + `${trgtPath}/${result}`);
-      vscode.workspace.fs.copy(source, target, {overwrite: false}).then(_ => {
-
-        // Open the README.md of the newly copied proof
-        let rdmePath = vscode.Uri.parse(`${target}/README.md`).fsPath;
-        vscode.workspace.openTextDocument(rdmePath).then((td: vscode.TextDocument) => {
-          vscode.window.showTextDocument(td, 1, false);
-        }, (error) => {
-          vscode.window.showErrorMessage(error);
-        });
-
-      }, (_) => {
-        vscode.window.showErrorMessage('Could not copy proof, maybe it already exists');
-      });
-
-    } else {
-      vscode.window.showErrorMessage("Extension path not found, try reinstalling");
+    if (!vscode.workspace.workspaceFolders) {
+      const message = 'Could not find working folder, open a folder and try again';
+      vscode.window.showErrorMessage(message);
       return;
-    }    
+    }
+    const targetPath = `${vscode.workspace.workspaceFolders[0].uri.path}/${targetDir}`;
+
+    const selectedProof = await vscode.window.showQuickPick(proofsList, {
+      placeHolder: 'Select a Proof Point',
+    });
+    if (!selectedProof) { return; }
+    vscode.window.showInformationMessage(`Selected ${selectedProof}`);
+    const sourceProofDir: vscode.Uri = vscode.Uri.parse(`${proofsPath}/${selectedProof}`);
+    const targetProofDir: vscode.Uri = vscode.Uri.parse(`${targetPath}/${selectedProof}`);
+
+    copyProofThenOpenReadme(sourceProofDir, targetProofDir, doOverwrite);
 
   });
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(proofPointCommand);
 }
 
 export function deactivate() {}
